1f835da9074a598daa01674728cddb6cb359612d
watanabe moe <s1318262@cst.otsuma.ac.jp>
2020-05-24 15:52:01 +0900

    5/22課題

---
+++ php01/post_confirm.php
@@ -0,0 +1,48 @@
+<?php
+function h($value){
+    return htmlspecialchars($value, ENT_QUOTES);
+}
+
+
+$flg = 0;
+$name = $_POST["name"];
+$mail = $_POST["mail"];
+$s = $name.",".$mail;
+
+//File書き込み
+//課題は名前とメールの変数をカンマ区切りの文字列にしてファイルに書き込めるようにする。
+$file = fopen("data/data.txt","a");	// ファイル読み込み
+fwrite($file, $s."\r\n");
+fclose($file);
+
+/*if($name==""){
+    $name = "未入力です";
+    $flg = 1;
+}
+if($mail==""){
+    $mail = "未入力です";
+    $flg = 1;
+}*/
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>POST（受信）</title>
+</head>
+<body>
+
+お名前：<?php echo h($name); ?>
+EMAIL：<?php echo h($mail); ?>
+
+<?php 
+    if($flg == 0){ 
+?>
+    <button>登録</button>
+<?php 
+    }
+?>
+<ul>
+<li><a href="index.php">index.php</a></li>
+</ul>
+</body>
+</html>
